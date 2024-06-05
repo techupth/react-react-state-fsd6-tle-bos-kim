06a8062b5ab25eebcefa9a753cd055209c046d8d
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-06-05 11:27:03 +0700

    feat: add change greeting onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  function handleClick(text) {
+    return function () {
+      setGreeting(text);
+    };
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleClick("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={handleClick("Hi!")}>Hi!</button>
+        <button onClick={handleClick("你好!")}>你好!</button>
       </div>
     </div>
   );
