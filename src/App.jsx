d8c5ab97f8070a43ebc6edcbfe6f3f25707afc7e
tle <tlemakuz@gmail.com>
2024-06-06 18:11:00 +0700

    feat:state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import { useState  } from "react";
+
 
 function App() {
+ 
+  const firstGreeting = "Greeting Message"
+  const [isGreeting, changeGreeting] = useState(firstGreeting);
+  const changeSet = (eventGetdata) => {
+      changeGreeting(eventGetdata);
+      setTimeout(() => {changeGreeting(firstGreeting)}, 1500);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{isGreeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => changeSet('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={() => changeSet('Hi!')}>Hi!</button>
+        <button onClick={() => changeSet('你好!')}>你好!</button>
       </div>
     </div>
   );
